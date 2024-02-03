6c8c0cd8c662b5cd10265812201a7a94613842d0
Chyoppi <139049087+Chyoppi@users.noreply.github.com>
2024-02-03 22:18:54 +0200

    Add files via upload

---
+++ #DesktopArranger.py
@@ -0,0 +1,19 @@
+#DesktopArranger
+
+import os 
+
+#Desktop Location and where the files will be move to
+source_folder = r"C:\Users\(USER)\\Desktop"
+destination_folder = r"C:\Users\(USER)\Desktop\\Arrange"
+
+#Files we want to have on our desktop
+allowed_files = ["Files you want to add.txt"]
+
+allfiles = os.listdir(source_folder)
+
+#Goes through all the files and if they are not on the allowed list will be moved to destination
+for file in allfiles:
+    if file not in allowed_files:
+        src_path = os.path.join(source_folder, file)
+        dst_path = os.path.join(destination_folder, file)
+        os.rename(src_path, dst_path)
